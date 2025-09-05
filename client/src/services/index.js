0542bc19fe20907d465a94d04b886d77c42542c9
chennuboyinarajyalakshmi <chennuboinarajyalakshmi9@gmail.com>
2025-09-05 01:58:48 -0400

    Update index.js

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,77 +1,46 @@
 import axios from "axios";
 
-export const callRegisterUserApi = async (formData) => {
-  const response = await axios.post(
-    "http://localhost:5000/api/user/register",
-    formData,
-    { withCredentials: true }
-  );
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true, // if cookies/session needed
+});
 
+export const callRegisterUserApi = async (formData) => {
+  const response = await API.post("/api/user/register", formData);
   return response?.data;
 };
 
 export const callLoginUserApi = async (formData) => {
-  const response = await axios.post(
-    "http://localhost:5000/api/user/login",
-    formData,
-    { withCredentials: true }
-  );
-
+  const response = await API.post("/api/user/login", formData);
   return response?.data;
 };
 
 export const callUserAuthApi = async () => {
-  const response = await axios.post(
-    "http://localhost:5000/api/user/auth",
-    {},
-    { withCredentials: true }
-  );
-
-  console.log(response, "response");
-
+  const response = await API.post("/api/user/auth", {});
   return response?.data;
 };
 
-export const callLogoutApi = async (req, res) => {
-  const response = await axios.post(
-    "http://localhost:5000/api/user/logout",
-    {},
-    { withCredentials: true }
-  );
-
+export const callLogoutApi = async () => {
+  const response = await API.post("/api/user/logout", {});
   return response?.data;
 };
 
 export const addNewTaskApi = async (formData) => {
-  const response = await axios.post(
-    "http://localhost:5000/api/task/add-new-task",
-    formData
-  );
-
+  const response = await API.post("/api/task/add-new-task", formData);
   return response?.data;
 };
 
 export const getAllTasksApi = async (getCurrentUserId) => {
-  const response = await axios.get(
-    `http://localhost:5000/api/task/get-all-tasks-by-userid/${getCurrentUserId}`
-  );
-
+  const response = await API.get(`/api/task/get-all-tasks-by-userid/${getCurrentUserId}`);
   return response?.data;
 };
 
 export const updateTaskApi = async (formData) => {
-  const response = await axios.put(
-    `http://localhost:5000/api/task/update-task`,
-    formData
-  );
-
+  const response = await API.put("/api/task/update-task", formData);
   return response?.data;
 };
 
 export const deleteTaskApi = async (getCurrentTaskId) => {
-  const response = await axios.delete(
-    `http://localhost:5000/api/task/delete-task/${getCurrentTaskId}`
-  );
-
+  const response = await API.delete(`/api/task/delete-task/${getCurrentTaskId}`);
   return response?.data;
 };
