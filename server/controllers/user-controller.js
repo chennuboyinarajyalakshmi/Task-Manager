41609d31ef12858715433f1c1e71afb4c3aa2c71
chennuboyinarajyalakshmi <chennuboinarajyalakshmi9@gmail.com>
2025-09-05 05:56:25 -0400

    Update user-controller.js

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,88 +1,105 @@
-const Joi = require("joi");
-const User = require("../models/user");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
-// Schemas
-const registerSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
-
-const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
-
-// JWT token generator
-const generateToken = (userId) => {
-  return jwt.sign({ userId }, "DEFAULT_SECRET_KEY", { expiresIn: "3d" });
-};
-
-// Register user
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   const { error } = registerSchema.validate({ name, email, password });
+
   if (error) {
-    return res.status(400).json({ success: false, message: error.details[0].message });
+    return res.status(400).json({
+      success: false,
+      message: error.details[0].message,
+    });
   }
 
   try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ success: false, message: "Email already exists" });
+    const isUserEmailAlreadyExists = await User.findOne({ email });
+
+    if (isUserEmailAlreadyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User email already exists! Please try with a different email",
+      });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+    const hashPassword = await bcrypt.hash(password, 12);
 
-    const newUser = await User.create({ name, email, password: hashedPassword });
-    const token = generateToken(newUser._id);
+    const newlyCreatedUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
 
-    res.cookie("token", token, { httpOnly: true, withCredentials: true });
+    const token = generateToken(newlyCreatedUser._id);
+
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: true, // safer
+    });
 
     return res.status(201).json({
       success: true,
-      message: "User registered successfully",
-      userData: { name: newUser.name, email: newUser.email, _id: newUser._id },
+      message: "User registration successful",
+      userData: {
+        name: newlyCreatedUser.name,
+        email: newlyCreatedUser.email,
+        _id: newlyCreatedUser._id,
+      },
     });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ success: false, message: "Server error" });
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
   }
 };
 
-// Login user
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   const { error } = loginSchema.validate({ email, password });
+
   if (error) {
-    return res.status(400).json({ success: false, message: error.details[0].message });
+    return res.status(400).json({
+      success: false,
+      message: error.details[0].message,
+    });
   }
 
   try {
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ success: false, message: "Incorrect email" });
+    const getUser = await User.findOne({ email });
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if (!isPasswordCorrect) return res.status(400).json({ success: false, message: "Incorrect password" });
+    if (!getUser) {
+      return res.status(400).json({
+        message: "Incorrect email",
+        success: false,
+      });
+    }
 
-    const token = generateToken(user._id);
-    res.cookie("token", token, { httpOnly: true, withCredentials: true });
+    const checkAuth = await bcrypt.compare(password, getUser.password);
 
-    return res.status(200).json({ success: true, message: "User logged in" });
+    if (!checkAuth) {
+      return res.status(400).json({
+        message: "Incorrect password",
+        success: false,
+      });
+    }
+
+    const token = generateToken(getUser._id);
+
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: true,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in",
+    });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ success: false, message: "Server error" });
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
   }
 };
-
-// Logout
-const logout = (req, res) => {
-  res.cookie("token", "", { httpOnly: true, withCredentials: true });
-  return res.status(200).json({ success: true, message: "Logged out successfully" });
-};
-
-module.exports = { registerUser, loginUser, logout };
