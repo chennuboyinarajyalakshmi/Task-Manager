487c399ab623bd0ccd7b18acc9bcd6d700b12a82
chennuboyinarajyalakshmi <chennuboinarajyalakshmi9@gmail.com>
2025-09-05 06:19:48 -0400

    Update user-controller.js

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,5 +1,31 @@
-const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+const Joi = require("joi");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//to register an user
+//to login an user
+//logout
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const generateToken = (getId) => {
+  return jwt.sign({ getId }, "DEFAULT_SECRET_KEY", {
+    expiresIn: 3 * 24 * 60 * 60,
+  });
+};
+
+const registerUser = async (req, res, next) => {
+  const { name, email, password } = await req.body;
 
   const { error } = registerSchema.validate({ name, email, password });
 
@@ -16,36 +42,41 @@ const registerUser = async (req, res) => {
     if (isUserEmailAlreadyExists) {
       return res.status(400).json({
         success: false,
-        message: "User email already exists! Please try with a different email",
+        message: "User email already exists! Please try with different email",
       });
+    } else {
+      const hashPassword = await bcrypt.hash(password, 12);
+
+      const newlyCreatedUser = await User.create({
+        name,
+        email,
+        password: hashPassword,
+      });
+
+      if (newlyCreatedUser) {
+        const token = generateToken(newlyCreatedUser?._id);
+
+        res.cookie("token", token, {
+          withCredentials: true,
+          httpOnly: false,
+        });
+
+        res.status(201).json({
+          success: true,
+          message: "User registration successful",
+          userData: {
+            name: newlyCreatedUser.name,
+            email: newlyCreatedUser.email,
+            _id: newlyCreatedUser._id,
+          },
+        });
+
+        next();
+      }
     }
+  } catch (error) {
+    console.log(error);
 
-    const hashPassword = await bcrypt.hash(password, 12);
-
-    const newlyCreatedUser = await User.create({
-      name,
-      email,
-      password: hashPassword,
-    });
-
-    const token = generateToken(newlyCreatedUser._id);
-
-    res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: true, // safer
-    });
-
-    return res.status(201).json({
-      success: true,
-      message: "User registration successful",
-      userData: {
-        name: newlyCreatedUser.name,
-        email: newlyCreatedUser.email,
-        _id: newlyCreatedUser._id,
-      },
-    });
-  } catch (err) {
-    console.error(err);
     return res.status(500).json({
       success: false,
       message: "Something went wrong! Please try again",
@@ -53,8 +84,8 @@ const registerUser = async (req, res) => {
   }
 };
 
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+const loginUser = async (req, res, next) => {
+  const { password, email } = await req.body;
 
   const { error } = loginSchema.validate({ email, password });
 
@@ -69,7 +100,7 @@ const loginUser = async (req, res) => {
     const getUser = await User.findOne({ email });
 
     if (!getUser) {
-      return res.status(400).json({
+      return res.json({
         message: "Incorrect email",
         success: false,
       });
@@ -78,28 +109,44 @@ const loginUser = async (req, res) => {
     const checkAuth = await bcrypt.compare(password, getUser.password);
 
     if (!checkAuth) {
-      return res.status(400).json({
+      return res.json({
         message: "Incorrect password",
         success: false,
       });
     }
 
-    const token = generateToken(getUser._id);
-
+    const token = generateToken(getUser?._id);
     res.cookie("token", token, {
       withCredentials: true,
-      httpOnly: true,
+      httpOnly: false,
     });
 
-    return res.status(200).json({
+    res.status(201).json({
       success: true,
       message: "User logged in",
     });
-  } catch (err) {
-    console.error(err);
+
+    next();
+  } catch (error) {
+    console.log(error);
+
     return res.status(500).json({
       success: false,
       message: "Something went wrong! Please try again",
     });
   }
 };
+
+const logout = async (req, res) => {
+  res.cookie("token", "", {
+    withCredentials: true,
+    httpOnly: false,
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "Logout successfully",
+  });
+};
+
+module.exports = { registerUser, loginUser, logout };
