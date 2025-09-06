8d26817a35a5a9f81bb1ea84b31fc9d41424c9a2
chennuboyinarajyalakshmi <chennuboinarajyalakshmi9@gmail.com>
2025-09-06 02:24:53 -0400

    Update user-routes.js

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -10,7 +10,15 @@ const { userAuthVerification } = require("../middleware/auth-middlware");
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.post("/auth", userAuthVerification);
+userRouter.post("/auth", userAuthVerification, (req, res) => {
+  res.json({
+    success: true,
+    message: "User authenticated",
+    user: req.user,   // comes from middleware
+  });
+});
+
 userRouter.post("/logout", logout);
+
 
 module.exports = userRouter;
