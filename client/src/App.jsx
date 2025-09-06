c7a0c8d16fb332865c18d81fb7968c075f709e3b
chennuboyinarajyalakshmi <chennuboinarajyalakshmi9@gmail.com>
2025-09-06 05:53:11 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./pages/auth";
 import CommonLayout from "./components/common-layout";
@@ -8,13 +8,7 @@ import ScrumBoardPage from "./pages/scrum-board";
 function App() {
   return (
     <Routes>
-      {/* Default route → redirect to auth */}
-      <Route path="/" element={<Navigate to="/auth" />} />
-
-      {/* Auth route */}
       <Route path="/auth" element={<AuthPage />} />
-
-      {/* Protected routes under /tasks */}
       <Route path="/tasks" element={<CommonLayout />}>
         <Route path="list" element={<TasksPage />} />
         <Route path="scrum-board" element={<ScrumBoardPage />} />
