fe14370c719275a50d77df264ca3df1183befd90
chennuboyinarajyalakshmi <chennuboinarajyalakshmi9@gmail.com>
2025-10-22 07:05:26 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,22 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Task from "./pages/Task";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import { saveProfile } from "./redux/actions/authActions";
-import NotFound from "./pages/NotFound";
+import axios from "axios";
 
-function App() {
+export const saveProfile = (token) => async (dispatch) => {
+  const API_URL = process.env.REACT_APP_API_URL;
+  try {
+    const res = await axios.get(`${API_URL}/api/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  const authState = useSelector(state => state.authReducer);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) return;
-    dispatch(saveProfile(token));
-  }, [authState.isLoggedIn, dispatch]);
-
-
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={authState.isLoggedIn ? <Navigate to="/" /> : <Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/tasks/add" element={authState.isLoggedIn ? <Task /> : <Navigate to="/login" state={{ redirectUrl: "/tasks/add" }} />} />
-          <Route path="/tasks/:taskId" element={authState.isLoggedIn ? <Task /> : <Navigate to="/login" state={{ redirectUrl: window.location.pathname }} />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-}
-
-export default App;
+    dispatch({
+      type: "SAVE_PROFILE_SUCCESS",
+      payload: res.data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: "SAVE_PROFILE_FAILURE",
+      payload: error.response?.data?.msg || "Failed to load profile",
+    });
+  }
+};
