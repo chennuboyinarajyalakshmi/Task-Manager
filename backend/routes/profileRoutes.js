3985387ebfb534fc24ffa761c57ac3823541de88
chennuboyinarajyalakshmi <chennuboinarajyalakshmi9@gmail.com>
2025-10-22 07:47:35 -0400

    Update profileRoutes.js

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getProfile } = require("../controllers/profileControllers");
-const { verifyAccessToken } = require("../middlewares.js");
+const { verifyAccessToken } = require("../middlewares");
 
-// Routes beginning with /api/profile
+// GET /api/profile
 router.get("/", verifyAccessToken, getProfile);
 
 module.exports = router;
